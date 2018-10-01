d433cb2c897b3f50b07a0a58c99c295869afca52
Austin McGee <>
2018-10-01 12:50:01 +0200

    Render vector-tiled orgunits on the map.

--- src/js/map.js
+++ src/js/map.js
@@ -5,3 +5,57 @@ const map = new mapboxgl.Map({
     style: 'mapbox://styles/mapbox/streets-v9'
 });
 
+map.on('load', function () {
+    map.addLayer({
+        "id": "sierraleone-facilities",
+        "type": "circle",
+        "source": {
+            type: 'vector',
+            tiles: ["http://localhost:5000/sierraleone-orgunits/{z}/{x}/{y}"]
+        },
+        "source-layer": "facility",
+        // "layout": {
+        //     "line-join": "round",
+        //     "line-cap": "round"
+        // },
+        "paint": {
+            "circle-color": "#0000FF",
+            "circle-radius": 4,
+            "circle-blur": 1,
+        }
+    });
+    map.addLayer({
+        "id": "sierraleone-chiefdoms",
+        "type": "line",
+        "source": {
+            type: 'vector',
+            tiles: ["http://localhost:5000/sierraleone-orgunits/{z}/{x}/{y}"]
+        },
+        "source-layer": "chiefdom",
+        // "layout": {
+        //     "line-join": "round",
+        //     "line-cap": "round"
+        // },
+        "paint": {
+            "line-color": "#00FF00",
+            "line-width": 1
+        }
+    });
+    map.addLayer({
+        "id": "sierraleone-districts",
+        "type": "line",
+        "source": {
+            type: 'vector',
+            tiles: ["http://localhost:5000/sierraleone-orgunits/{z}/{x}/{y}"]
+        },
+        "source-layer": "district",
+        // "layout": {
+        //     "line-join": "round",
+        //     "line-cap": "round"
+        // },
+        "paint": {
+            "line-color": "#FF0000",
+            "line-width": 2
+        }
+    });
+});
