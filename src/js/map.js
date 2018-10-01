a877ab4940708d00948d4f0f2ba9c030f4f2f454
Bjørn Sandvik <bjorn@mastermaps.com>
2018-10-01 14:43:24 +0200

    Heatmap

--- src/js/map.js
+++ src/js/map.js
@@ -92,8 +92,6 @@ const toGeoJson = rows => ({
 });
 
 const createClusters = (data) => {
-    console.log('createClusters', data);
-
     map.addSource('events', { 
         type: 'geojson', 
         data,
@@ -155,13 +153,98 @@ const clearClusters = () => {
     map.removeLayer('unclustered-point');
 };
 
+createHeatmap = (data) => {
+    map.addSource('events', {
+        type: 'geojson',
+        data
+    });
+
+    map.addLayer({
+        "id": "events-heat",
+        "type": "heatmap",
+        "source": "events",
+        "maxzoom": 12,
+        "paint": {
+            // Increase the heatmap weight based on frequency and property magnitude
+            "heatmap-weight": 0.2,
+            /*
+            "heatmap-weight": [
+                "interpolate",
+                ["linear"],
+                ["get", "mag"],
+                0, 0,
+                6, 1
+            ],
+            */
+            // Increase the heatmap color weight weight by zoom level
+            // heatmap-intensity is a multiplier on top of heatmap-weight
+            "heatmap-intensity": [
+                "interpolate",
+                ["linear"],
+                ["zoom"],
+                0, 1,
+                12, 3
+            ],
+            // Color ramp for heatmap.  Domain is 0 (low) to 1 (high).
+            // Begin color ramp at 0-stop with a 0-transparancy color
+            // to create a blur-like effect.
+            "heatmap-color": [
+                "interpolate",
+                ["linear"],
+                ["heatmap-density"],
+                0, "rgba(33,102,172,0)",
+                0.2, "rgb(103,169,207)",
+                0.4, "rgb(209,229,240)",
+                0.6, "rgb(253,219,199)",
+                0.8, "rgb(239,138,98)",
+                1, "rgb(178,24,43)"
+            ],
+            // Adjust the heatmap radius by zoom level
+            "heatmap-radius": [
+                "interpolate",
+                ["linear"],
+                ["zoom"],
+                0, 1,
+                12, 10
+            ],
+            // Transition from heatmap to circle layer by zoom level
+            "heatmap-opacity": [
+                "interpolate",
+                ["linear"],
+                ["zoom"],
+                7, 1,
+                12, 1
+            ],
+        }
+    }, 'waterway-label');
+
+    map.addLayer({
+        "id": "events-point",
+        "type": "circle",
+        "source": "events",
+        "minzoom": 10,
+        "paint": {
+            // Size circle radius by earthquake magnitude and zoom level
+            "circle-radius": 4,
+            // Color circle by earthquake magnitude
+            "circle-color": 'red',
+            "circle-stroke-color": "white",
+            "circle-stroke-width": 1,
+            // Transition from heatmap to circle layer by zoom level
+            "circle-opacity": 1
+        }
+    }, 'waterway-label');
+
+    console.log('heatmap');
+};
+
 const init = async () => {
-    console.log('load data');
     const data = await getData(malariaEvents);
     const geoJson = toGeoJson(data.rows);
 
     addVectorLayers();
-    createClusters(geoJson);
+    // createClusters(geoJson);
+    createHeatmap(geoJson);
 
 }
 
