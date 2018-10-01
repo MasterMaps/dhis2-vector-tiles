ec49344f0c1d09ae24140358aff27239ed1f15f7
Bjørn Sandvik <bjorn@mastermaps.com>
2018-10-01 11:24:31 +0200

    Initial commit

---
+++ src/js/map.js
@@ -0,0 +1,7 @@
+mapboxgl.accessToken = 'pk.eyJ1IjoibWFzdGVybWFwcyIsImEiOiJjaXdiYnZlaDgwMDNuMnZucm40dG1jZjd2In0.2oOUnZaKYWuZyz35I4rD9g';
+
+const map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/mapbox/streets-v9'
+});
+
