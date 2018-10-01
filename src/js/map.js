960507f19560768cb7cdcd28b0d27b8a04f43b0b
Austin McGee <>
2018-10-01 18:37:32 +0200

    Add server tile heatmaps (generated client-side) and layer toggle buttons.

--- src/js/map.js
+++ src/js/map.js
@@ -19,19 +19,31 @@ const addVectorLayers = () => {
         type: 'vector',
         tiles: [`${serverTiles}sierraleone-orgunits/{z}/{x}/{y}`],
     });
+    map.addSource('serverMalaria', {
+        type: 'vector',
+        tiles: [`${serverTiles}malaria/{z}/{x}/{y}`],
+    });
     map.addLayer({
         "id": "sierraleone-facilities",
         "type": "circle",
         "source": 'orgUnits',
         "source-layer": "facility",
+        "minzoom": 7, // Zoom levels not visible on init fail for some reason...
         // "layout": {
         //     "line-join": "round",
         //     "line-cap": "round"
         // },
         "paint": {
             "circle-color": "#0000FF",
-            "circle-radius": 4,
-            "circle-blur": 1,
+            "circle-radius": 5,
+            "circle-opacity": [
+                "interpolate",
+                ["linear"],
+                ["zoom"],
+                7, 0,
+                9, 0,
+                10, 1,
+            ]
         }
     });
     map.addLayer({
@@ -43,9 +55,17 @@ const addVectorLayers = () => {
         //     "line-join": "round",
         //     "line-cap": "round"
         // },
+        minzoom: 7, // Zoom levels not visible on init fail for some reason...
         "paint": {
-            "line-color": "#00FF00",
-            "line-width": 1
+            "line-color": "#999999",
+            "line-width": 2,
+            "line-opacity": [
+                "interpolate",
+                ["linear"],
+                ["zoom"],
+                7, 0,
+                9, 1,
+            ]
         }
     });
     map.addLayer({
@@ -58,8 +78,101 @@ const addVectorLayers = () => {
         //     "line-cap": "round"
         // },
         "paint": {
-            "line-color": "#FF0000",
-            "line-width": 2
+            "line-color": "#333333",
+            "line-width": 2,
+        }
+    });
+
+    map.addLayer({
+        "id": "sierraleone-malaria-pts",
+        "type": "circle",
+        "source": 'serverMalaria',
+        "source-layer": "malariaevents",
+        minzoom: 7, // Zoom levels not visible on init fail for some reason...
+        // "layout": {
+        //     "line-join": "round",
+        //     "line-cap": "round"
+        // },
+        "paint": {
+            "circle-radius": [
+                "interpolate",
+                ["linear"],
+                ["zoom"],
+                7, 2,
+                16, 4
+            ],
+            "circle-color": "#FF0000",
+            "circle-stroke-width": 1,
+            "circle-stroke-color": "#fff",
+            "circle-opacity": [
+                "interpolate",
+                ["linear"],
+                ["zoom"],
+                7, 0,
+                9, 0,
+                14, 1
+            ],
+            "circle-stroke-opacity": [
+                "interpolate",
+                ["linear"],
+                ["zoom"],
+                7, 0,
+                9, 0,
+                14, 1
+            ]
+        }
+    });
+    map.addLayer({
+        "id": "sierraleone-malaria-heatmap",
+        "type": "heatmap",
+        "source": 'serverMalaria',
+        "source-layer": "malariaevents",
+        "maxzoom": 14,
+        // "layout": {
+        //     "line-join": "round",
+        //     "line-cap": "round"
+        // },
+        "paint": {
+            "heatmap-opacity": [
+                "interpolate",
+                ["linear"],
+                ["zoom"],
+                9, 0.8,
+                11, 0.2,
+                14, 0
+            ],
+            "heatmap-weight": [
+                "interpolate",
+                ["linear"],
+                ["get", "mag"],
+                0, 0,
+                6, 1
+            ],
+            "heatmap-intensity": [
+                "interpolate",
+                ["linear"],
+                ["zoom"],
+                0, 1,
+                11, 3
+            ],
+            "heatmap-color": [
+                "interpolate",
+                ["linear"],
+                ["heatmap-density"],
+                0, "rgba(33,102,172,0)",
+                0.2, "rgb(103,169,207)",
+                0.4, "rgb(209,229,240)",
+                0.6, "rgb(253,219,199)",
+                0.8, "rgb(239,138,98)",
+                1, "rgb(178,24,43)"
+            ],
+            "heatmap-radius": [
+                "interpolate",
+                ["linear"],
+                ["zoom"],
+                0, 2,
+                11, 20
+            ],
         }
     });
 };
@@ -86,12 +199,13 @@ const toGeoJson = rows => ({
     })),
 });
 
-const createClusters = (data) => {
-    console.log('createClusters', data);
+const createClusters = async () => {
+    const data = await getData(malariaEvents);
+    const geoJson = toGeoJson(data.rows);
 
     map.addSource('events', { 
         type: 'geojson', 
-        data,
+        data: geoJson,
         cluster: true,
         clusterMaxZoom: 14,
         clusterRadius: 50
@@ -144,14 +258,57 @@ const createClusters = (data) => {
 };
 
 const init = async () => {
-    console.log('load data');
-    const data = await getData(malariaEvents);
-    const geoJson = toGeoJson(data.rows);
-
     addVectorLayers();
-    createClusters(geoJson);
-
+    await createClusters();
 }
 
 map.on('load', init);
 
+function createLayerToggles() {
+    var toggleableLayerGroups = {
+        'Server Heatmap': [
+           'sierraleone-malaria-heatmap',
+           'sierraleone-malaria-pts', 
+        ],
+        'Client Clusters': [
+            'clusters',
+            'cluster-count',
+            'unclustered-point'
+        ]
+    };
+
+    var groups = Object.keys(toggleableLayerGroups);
+    for (var i = 0; i < groups.length; i++) {
+        var name = groups[i];
+
+        var link = document.createElement('a');
+        link.href = '#';
+        link.className = 'active';
+        link.textContent = name;
+
+        var themap = map;
+        link.onclick = function (e) {
+            var ids = toggleableLayerGroups[this.textContent];
+            e.preventDefault();
+            e.stopPropagation();
+
+            for (var i = 0; i < ids.length; ++i) {
+                const id = ids[i];
+                var visibility = themap.getLayoutProperty(id, 'visibility');
+
+                if (visibility === 'visible') {
+                    themap.setLayoutProperty(id, 'visibility', 'none');
+                    this.className = '';
+                } else {
+                    this.className = 'active';
+                    themap.setLayoutProperty(id, 'visibility', 'visible');
+                }
+            }
+        };
+
+        var layers = document.getElementById('menu');
+        layers.appendChild(link);
+    }
+}
+
+createLayerToggles();
